6f42d109c2c32c7972622f027c92776f77d8e95d
Andrea Williams <anw0514@gmail.com>
2018-12-27 14:50:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(speech)
   elsif speech.upcase == speech
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH? SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
