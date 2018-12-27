1bda071307f9f848349e59eb5624007c4bd6999b
Andrea Williams <anw0514@gmail.com>
2018-12-27 14:36:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(speech)
-  if speech.upcase == speech
-    puts "NO, NOT SINCE 1938!"
-  elsif speech == "I LOVE YOU GRANDMA!"
+  if speech == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif speech.upcase == speech
+    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH? SPEAK UP, SONNY!"
   end
